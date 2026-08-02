a1120bfe24222696d727106f91ae3464d27ff528
ZERONE2077 <biulibiuli@outlook.com>
2026-08-03 04:38:57 +0800

    Add remote launch shortcuts and Lua assets

---
+++ Lua/2001120.lua
@@ -0,0 +1,20 @@
+-- 2001120's Lua and Manifest Created by Hubcap Manifest
+-- Split Fiction
+-- Created: September 30, 2025 at 19:10:21 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2001120) -- Split Fiction
+-- MAIN APP DEPOTS
+addappid(2001121, 1, "27f6aac2ce9d80ad4ed8fe3710d306ae108f00ddcf005d47ecf7bfa2a258964c") -- Depot 2001121
+setManifestid(2001121, "4899075162680269256", 94109488417)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3050960) -- Split Fiction - Full Game Unlock
---
+++ Lua/2483190.lua
@@ -0,0 +1,47 @@
+-- 2483190's Lua and Manifest Created by Hubcap Manifest
+-- Forza Horizon 6
+-- Created: July 15, 2026 at 05:53:39 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 3
+-- Total DLCs: 21 (2 excluded)
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2483190, 1, "bca2f0f9973d929c2c6bae884058b8bf7934e9ecc8ff34d584450dfbb3215040") -- Forza Horizon 6
+-- MAIN APP DEPOTS
+addappid(2483191, 1, "f2a09f0a31b4df681ec0d2d572f368dfa4cd6c37ae67ebdfd591cfbe1334fcd9") -- Depot 2483191
+setManifestid(2483191, "2541360848233493756", 167216227751)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4439260) -- Forza Horizon 6 Welcome Pack
+addappid(4439270) -- Forza Horizon 6 Premium VIP
+addappid(4439280) -- Forza Horizon 6 Time Attack Car Pack
+addappid(4439290) -- Forza Horizon 6 Italian Passion Car Pack
+addtoken(4439290, "7027096395897994901")
+addappid(4439300) -- Forza Horizon 6 Ferrari J50
+addappid(4439750) -- Forza Horizon 6 Treasure Map
+addappid(4439790) -- Forza Horizon 6 Car Pass
+addappid(4439800) -- Forza Horizon 6 1990 Nissan 12 Skyline GT-R (BNR32 Gr.A) JTC
+addappid(4439810) -- Forza Horizon 6 2024 Koenigsegg Gemera
+addappid(4439820) -- Forza Horizon 6 1972 Datsun 269 Attacking the Clock Racing 240Z All Carbon Hill Climb Beast
+addappid(4439830) -- Forza Horizon 6 2008 Honda Civic Type R (FD2)
+addappid(4439840) -- Forza Horizon 6 2023 Audi R8 Coup V10 GT RWD
+addappid(4439890) -- Forza Horizon 6 1974 Mazda 123 Mad Mike 808 Wagon FURSTY
+addappid(4439900) -- Forza Horizon 6 2023 Toyota GR Corolla
+addappid(4440380) -- Forza Horizon 6 VIP Membership
+addappid(4444140) -- Forza Horizon 6 TBDS2
+addappid(4444150) -- Forza Horizon 6 1962 Peel P50 Trolli Edition
+addappid(4520350) -- Forza Horizon 6 Expansions Bundle
+addappid(4520360) -- Forza Horizon 6 Premium Upgrade
+addappid(4562050) -- Forza Horizon 6 Crunchyroll Voucher
+addappid(4610350) -- Forza Horizon 6 1998 Nissan Skyline GT-R 40th Anniversary
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(4439240) -- DLC 4439240 (unreleased)
+-- addtoken(4439240, "10618922691696453695")
+-- addappid(4439250) -- DLC 4439250 (unreleased)
+-- addtoken(4439250, "6382430189070780048")
